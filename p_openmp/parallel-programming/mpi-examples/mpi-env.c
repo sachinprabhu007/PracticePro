823eb7ad0d03183f7644da7f50d5308745bcc924
sachinprabhu007 <sachin.prabhu.b@gmail.com>
2017-02-23 15:11:29 +0530

    update

---
+++ p_openmp/parallel-programming/mpi-examples/mpi-env.c
@@ -0,0 +1,30 @@
+/**
+ * MPI Environment Management test.
+ */
+
+#include "mpi.h"
+#include <stdio.h>
+
+int main( int argc, char *argv[] )
+{
+  int numtasks, myrank, len;
+  char hostname[ MPI_MAX_PROCESSOR_NAME ];
+  int version, subversion;
+
+  MPI_Init( &argc, &argv ); // must be called and should be called only once
+  MPI_Comm_size( MPI_COMM_WORLD, &numtasks ); // get number of MPI tasks
+  MPI_Comm_rank( MPI_COMM_WORLD, &myrank ); // get MPI rank for current MPI task
+  MPI_Get_processor_name( hostname, &len ); // get processor name running current MPI task
+
+  if ( myrank == 0 ) {
+    MPI_Get_version( &version, &subversion );
+    printf( "MPI standard version: %d.%d\n", version, subversion );
+    printf( "Number of task: %d\n", numtasks );
+    printf( "MPI_ANY_TAG = %d\n", MPI_ANY_TAG );
+    printf( "size of MPI_ANY_TAG = %ld\n", sizeof(MPI_ANY_TAG) );
+  }
+
+  printf( "Rank %d is running on %s\n", myrank, hostname );
+
+  MPI_Finalize();
+}
